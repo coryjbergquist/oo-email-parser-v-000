34360b67b88187746c0048fcdd343b9003ad5e2d
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-26 19:18:14 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,7 +9,7 @@ class EmailParser
   attr_accessor :email
 
   def initialize(argument)
-    name = argument.split(/[^a-zA-Z."]/)
+    name = argument.split(/[^a-zA-Z."@]/)
     @@all << name
 
   end
